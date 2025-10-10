1aaca24b8bafed3dbb42343fbacef84f7a6e1017
Mikolaj <rudkowski.dev@gmail.com>
2025-10-10 20:02:50 +0200

    clear front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import { AuthButton } from '@/components/auth-button'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import Link from 'next/link'
@@ -10,28 +9,18 @@ export default function Home() {
 				<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
 					<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
 						<div className='flex gap-5 items-center font-semibold'>
-							<Link href={'/'}>Next.js Supabase Starter</Link>
+							<Link href={'/'}>TrenUI</Link>
 						</div>
 						<AuthButton />
 					</div>
 				</nav>
 				<div className='flex-1 flex flex-col gap-20 max-w-5xl p-5'>
 					<main className='flex-1 flex flex-col gap-6 px-4'>
-						<h2 className='font-medium text-xl mb-4'>Next steps</h2>
+						<h2 className='font-medium text-xl mb-4'>Test Front Page</h2>
 					</main>
 				</div>
 
 				<footer className='w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16'>
-					<p>
-						Powered by{' '}
-						<a
-							href='https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs'
-							target='_blank'
-							className='font-bold hover:underline'
-							rel='noreferrer'>
-							Supabase
-						</a>
-					</p>
 					<ThemeSwitcher />
 				</footer>
 			</div>
