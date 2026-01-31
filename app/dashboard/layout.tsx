b9e0f657145cda8dfc0e83501e8d4871c7a6cd48
Mikolaj <rudkowski.dev@gmail.com>
2026-01-31 17:03:04 +0100

    fix layout styles

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,9 +5,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<main className='min-h-screen flex flex-col w-full p-4 gap-2'>
+			<main className='min-h-screen flex flex-col items-center w-full p-4 gap-2'>
 				<SidebarTrigger />
-				{children}
+				<div className='w-full max-w-7xl'>{children}</div>
 			</main>
 		</SidebarProvider>
 	)
