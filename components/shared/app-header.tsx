30d9fa2a8106f65dacf0310701dcfff3ad7c06ff
Mikolaj <rudkowski.dev@gmail.com>
2026-03-09 21:55:25 +0100

    add user button

--- components/shared/app-header.tsx
+++ components/shared/app-header.tsx
@@ -1,6 +1,7 @@
 import { ThemeSwitcher } from './theme-switcher'
 import { SidebarTrigger } from '../ui/sidebar'
 import { HeaderBreadcrumbs } from './header-breadcrumb'
+import { UserButton } from '@neondatabase/auth/react'
 
 export function AppHeader() {
 	return (
@@ -10,7 +11,10 @@ export function AppHeader() {
 				<div className='w-px h-4 bg-border' />
 				<HeaderBreadcrumbs />
 			</div>
-			<ThemeSwitcher />
+			<div className='flex items-center gap-2'>
+				<ThemeSwitcher />
+				<UserButton size='icon' />
+			</div>
 		</header>
 	)
 }
